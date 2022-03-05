980901a89d3324d9597bf5108ba535a47a29a8ab
dk949 <56653556+dk949@users.noreply.github.com>
2022-03-05 22:27:54 +0000

    (build): re-expoerting everything in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import process from "process";
-
-function main(): void {
-    process.stdout.write("hello\n");
-}
-
-main();
+export * as Cat from "./cat";
+export * as Echo from "./echo";
+export * as False from "./false";
+export * as Rm from "./rm";
+export * as Talse from "./true";
